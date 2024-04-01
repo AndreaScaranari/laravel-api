c232b62461cbe1086a04cada67e8a0a0bb5b226b
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-04-02 00:08:52 +0200

    controller_api_in_route

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -13,7 +13,8 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $projects = Project::all();
+        // $projects = Project::all();
+        $projects = Project::whereIsPublished(true)->latest()->get();
         return response()->json($projects);
     }
 
